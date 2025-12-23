0537aaeb7b61f696592fdcee8956bb022140aae6
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-12-23 13:15:15 +0000

    fix: tidy up some pulumi experimental code

--- components/__main__.py
+++
@@ -1,5 +0,0 @@
-from pulumi.provider.experimental import component_provider_host
-from src.components.ecr_repository import ECRRepository
-
-if __name__ == "__main__":
-    component_provider_host(name="ecr-repository-component", components=[ECRRepository])
