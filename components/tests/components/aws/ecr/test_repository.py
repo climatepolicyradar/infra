3f34032185df8318813ee6ca2ac5659ad656711e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-02-05 11:27:30 +0000

    Move

--- components/tests/components/aws/ecr/test_repository.py
+++ components/tests/components/aws/ecr/test_repository.py
@@ -1,8 +1,8 @@
 import json
 
 import pulumi
-from components.component.aws.ecr import Repository
-from components.component.aws.ecr.types import ResourceType
+from components.aws.ecr.repository import Repository
+from components.aws.ecr.types import ResourceType
 from pulumi.provider.experimental.analyzer import Analyzer
 
 
